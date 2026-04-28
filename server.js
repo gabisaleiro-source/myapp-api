aeab2c43c84a8f78fc93e42e9b562490e691a7d9
31372-blip <31372@aeaf.edu.pt>
2026-04-28 10:50:11 +0100

    fix: configurar servidor para Railway

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ mongoose.connect(MONGO_URI)
 
 // Porta
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Servidor a correr na porta ${PORT}`);
 });
